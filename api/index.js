1ab9010568f28aade11345863ec68c326acdd702
Gabriel Morin <gabriel.morin98@gmail.com>
2024-09-16 16:00:49 -0400

    2024/09/16 0.1

--- api/index.js
+++ api/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
-const routes = require('./routes');
 
 const PORT = 8000;
 
@@ -15,15 +14,14 @@ app.use(cors({
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(require('./routes'));
-app.use('/api', routes);
 
 // 404 for handling undefined routes
 app.use('*', (req, res) => {
   res.status(404).json({status: 404, message: "Désolé, vous ne trouverez pas de bonbons ici!"});
 });
 
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 module.exports = app;
