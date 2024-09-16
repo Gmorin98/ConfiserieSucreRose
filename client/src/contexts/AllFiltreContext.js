deb6a656c9438fb8f0f9ad01ee8ad5905c3fa7c4
Gabriel Morin <gabriel.morin98@gmail.com>
2024-09-16 16:50:20 -0400

    2024/09/16 0.4

--- client/src/contexts/AllFiltreContext.js
+++ client/src/contexts/AllFiltreContext.js
@@ -9,7 +9,7 @@ const AllFiltreProvider = ({children}) => {
   useEffect(() => {
     const fetchFiltreVracInfo = async () => {
       try {
-        const response = await fetch(`/${process.env.REACT_APP_API_URL}/getFiltre/Vrac`);
+        const response = await fetch(`${process.env.REACT_APP_API_URL}/getFiltre/Vrac`);
         if (!response.ok) {
           throw new Error("Failed to fetch Vrac Filtre");
         }
@@ -22,7 +22,7 @@ const AllFiltreProvider = ({children}) => {
 
     const fetchFiltreProduitsInfo = async () => {
       try {
-        const response = await fetch(`/${process.env.REACT_APP_API_URL}/getFiltre/Produits`);
+        const response = await fetch(`${process.env.REACT_APP_API_URL}/getFiltre/Produits`);
         if (!response.ok) {
           throw new Error("Failed to fetch Produits Filtre");
         }
