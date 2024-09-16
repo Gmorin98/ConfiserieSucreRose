e8df59f1d4f42ca8064929ccda00e3097bc3db14
Gabriel Morin <gabriel.morin98@gmail.com>
2024-09-16 18:01:32 -0400

    2024/09/16 0.6

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const routes = require('./routes');
+const bodyParser = require("body-parser");
 
 const PORT = 8000;
 
@@ -16,6 +17,7 @@ app.use(express.json());
 app.use(morgan("dev"));
 app.use(require('./routes'));
 app.use('/api', routes);
+app.use(bodyParser.json())
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
