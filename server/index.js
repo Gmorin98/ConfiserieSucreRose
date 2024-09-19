7e1f48eaa78e8915bd17da4a2208faa932fe78df
Gabriel Morin <gabriel.morin98@gmail.com>
2024-09-19 11:46:21 -0400

    2024/09/19 0.1

--- server/index.js
+++ server/index.js
@@ -1,18 +1,32 @@
+const cors = require("cors");
 const express = require("express");
 const morgan = require("morgan");
-const cors = require("cors");
 const routes = require('./routes');
 
 const PORT = 8000;
 
 const app = express();
 
+const whitelist = ['https://confiserie-sucre-rose-frontend.vercel.app', 'https://confiserie-sucre-rose-backend.vercel.app'];
 const corsOptions = {
-  origin: ['https://confiserie-sucre-rose-frontend.vercel.app', 'https://confiserie-sucre-rose-backend.vercel.app', 'http://localhost:8000'],
+  origin: function (origin, callback) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true, // If you need to include credentials like cookies
-};
+  credentials: true
+}
+
+// const corsOptions = {
+//   origin: ['https://confiserie-sucre-rose-frontend.vercel.app', 'https://confiserie-sucre-rose-backend.vercel.app', 'http://localhost:8000'],
+//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//   allowedHeaders: ['Content-Type', 'Authorization'],
+//   credentials: true, // If you need to include credentials like cookies
+// };
 
 app.use(cors(corsOptions));
 
