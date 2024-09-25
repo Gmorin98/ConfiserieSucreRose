b64e813272986e63bd28b24c781e5824511bc820
Gabriel Morin <gabriel.morin98@gmail.com>
2024-09-24 21:44:32 -0400

    2024/09/24 0.1 Added Html Email Format.

--- server/handlers/postConfirmationCommande.js
+++ server/handlers/postConfirmationCommande.js
@@ -1,7 +1,7 @@
 const AWS = require('@aws-sdk/client-ses');
-const stripe = require('stripe')(process.env.STRIPE_KEY)
-
+const stripe = require('stripe')(process.env.STRIPE_KEY);
 require("dotenv").config();
+
 const SES_CONFIG = {
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
@@ -14,43 +14,127 @@ const AWS_SES = new AWS.SES(SES_CONFIG);
 
 const postConfirmationCommande = async (req, res) => {
   const { panierWithoutImg, customerName, orderNumber, sessionId, customerEmail } = req.body;
-  
+
   try {
     const session = await stripe.checkout.sessions.retrieve(sessionId);
-
     const amountPaid = (session.amount_total / 100).toFixed(2); // Stripe amount is in cents
 
     // Format panier data with greeting and footer
-    let htmlBody = `<h1>Bonjour ${customerName},</h1><h2>Numéro de Commande: ${orderNumber}</h2><h2>Détails de la Commande</h2>`;
+    let htmlBody = `
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <style>
+            body {
+                font-family: Sacramento, cursive;
+                margin: 0;
+                padding: 0;
+            }
+            .email-container {
+                max-width: 600px;
+                width: 100%;
+                margin: 0 auto;
+                background-color: #FAE6E8;
+                color: #B63643;
+                padding: 20px;
+                box-shadow: 0 0 10px rgba(0,0,0,0.1);
+                border-radius: 15px;
+            }
+            h1, h2, ul {
+                color: #B63643;
+            }
+            ul {
+                padding-left: 20px;
+                display: flex;
+                flex-direction: column;
+            }
+            li {
+                list-style-type: none;
+            }
+            p {
+                font-size: 16px;
+                line-height: 1.5;
+                color: #B63643;
+            }
+            .items {
+                background-color: #ffffff;
+                padding: 5px 10px;
+                border-radius: 10px;
+            }
+            .footer {
+                font-size: 14px;
+                color: #B63643;
+                text-align: center;
+                margin-top: 20px;
+                padding-top: 20px;
+                border-top: 1px solid #B63643;
+            }
+            @media only screen and (max-width: 600px) {
+                .email-container {
+                    padding: 10px;
+                }
+                h1 {
+                    font-size: 24px;
+                }
+                h2 {
+                    font-size: 18px;
+                }
+            }
+        </style>
+    </head>
+    <body>
+        <div class="email-container">
+            <h1>Bonjour ${customerName},</h1>
+            <h2>Numéro de Commande: ${orderNumber}</h2>
+            <h2>Détails de la Commande</h2>
+            <div class="items">`;
+
+    // Generate order items
+    panierWithoutImg.forEach(item => {
+      if (item.bonbonsSelectionne) {
+        htmlBody += `<p>${item.nom} (Quantité: ${item.quantity})</p><ul>`;
+        item.bonbonsSelectionne.forEach(bonbon => {
+          htmlBody += `<li>${bonbon.nom} - Quantité: ${bonbon.quantite}g</li>`;
+        });
+        htmlBody += '</ul>';
+      } else {
+        htmlBody += `<p>${item.nom} - Quantité: ${item.quantity}</p>`;
+      }
+    });
+
+    // Append the footer and payment information
+    htmlBody += `
+            </div>
+            <p><strong>Montant Payé: $${amountPaid} CAD</strong></p>
+            <div class="footer">
+                *Toute commande sera prête dans un délai de 24 à 48 heures.
+            </div>
+        </div>
+    </body>
+    </html>`;
+
+    // Plain text version for fallback
     let textBody = `Bonjour ${customerName},\n\nNuméro de Commande: ${orderNumber}\nDétails de la Commande:\n`;
 
     panierWithoutImg.forEach(item => {
       if (item.bonbonsSelectionne) {
-        htmlBody += `<h2>${item.nom} (Quantité: ${item.quantity})</h2><ul>`;
         textBody += `${item.nom} (Quantité: ${item.quantity}):\n`;
-        
         item.bonbonsSelectionne.forEach(bonbon => {
-          htmlBody += `<li>${bonbon.nom} - Quantité: ${bonbon.quantite}</li>`;
-          textBody += `  - ${bonbon.nom} - Quantité: ${bonbon.quantite}\n`;
+          textBody += `  - ${bonbon.nom} - Quantité: ${bonbon.quantite}g\n`;
         });
-        
-        htmlBody += '</ul>';
       } else {
-        htmlBody += `<p>${item.nom} - Quantité: ${item.quantity}</p>`;
         textBody += `${item.nom} - Quantité: ${item.quantity}\n`;
       }
     });
 
-    htmlBody += `<p><strong>Total Payé: $${amountPaid} CAD</strong></p>`;
-
-    // Add footer
-    htmlBody += `<p><strong>Toute commande sera prête dans un délai de 24 à 48 heures.</strong></p>`;
-    textBody += `\nToute commande sera prête dans un délai de 24 à 48 heures.\n`;
+    textBody += `\nMontant Payé: $${amountPaid} CAD\n*Toute commande sera prête dans un délai de 24 à 48 heures.\n`;
 
     const params = {
       Source: 'confiseriesucrerose@gmail.com',
       Destination: {
-        ToAddresses: [`confiseriesucrerose@gmail.com`],
+        ToAddresses: [`${customerEmail}`],
       },
       Message: {
         Body: {
@@ -69,7 +153,7 @@ const postConfirmationCommande = async (req, res) => {
         },
       },
     };
-  
+
     const result = await AWS_SES.sendEmail(params);
     res.status(200).json({ status: 200, message: "Email sent successfully!", result });
   } catch (error) {
--- server/handlers/postNouvelleCommande.js
+++ server/handlers/postNouvelleCommande.js
@@ -23,30 +23,94 @@ const postNouvelleCommande = async (req, res) => {
     const phoneNumber = session.customer_details.phone; // Retrieve the phone number.
 
     // Format panier data
-    let htmlBody = '<h1>Nouvelle Commande en Ligne</h1>';
-    let textBody = 'Nouvelle Commande en Ligne:\n';
+    let htmlBody = `
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <style>
+            body {
+                font-family: Sacramento, cursive;
+                margin: 0;
+                padding: 0;
+            }
+            .email-container {
+                max-width: 600px;
+                width: 100%;
+                margin: 0 auto;
+                background-color: #FAE6E8;
+                color: #B63643;
+                padding: 20px;
+                box-shadow: 0 0 10px rgba(0,0,0,0.1);
+                border-radius: 15px;
+            }
+            h1, h2, ul {
+                color: #B63643;
+            }
+            ul {
+                padding-left: 20px;
+                display: flex;
+                flex-direction: column;
+            }
+            li {
+                list-style-type: none;
+            }
+            p {
+                font-size: 16px;
+                line-height: 1.5;
+                color: #B63643;
+            }
+            .items {
+                background-color: #ffffff;
+                padding: 5px 10px;
+                border-radius: 10px;
+            }
+            .footer {
+                font-size: 14px;
+                color: #B63643;
+                text-align: center;
+                margin-top: 20px;
+                padding-top: 20px;
+                border-top: 1px solid #B63643;
+            }
+            @media only screen and (max-width: 600px) {
+                .email-container {
+                    padding: 10px;
+                }
+                h1 {
+                    font-size: 24px;
+                }
+                h2 {
+                    font-size: 18px;
+                }
+            }
+        </style>
+    </head>
+    <body>
+        <div class="email-container">
+          <h1>Nouvelle Commande en Ligne</h1>
+          <h2>Information Commande</h2>
+          <p>Numéro de Commande: ${orderNumber}</p>
+          <p>Nom: ${customerName}</p>
+          <p>Courriel: ${customerEmail}</p>
+          <p>Numéro de Téléphone: ${phoneNumber}</p>
+          <p><strong>Total Payé: $${amountPaid} CAD</strong></p>
+          <div class="items">`
 
-    htmlBody += `<h2>Information Commande</h2>`;
-    textBody += `Information Commande:\n`;
-
-    htmlBody += `<p>Numéro de Commande: ${orderNumber}</p>`;
-    textBody += `Numéro de Commande: ${orderNumber}\n`;
-
-    htmlBody += `<p>Nom: ${customerName}</p>`;
-    textBody += `Nom: ${customerName}:\n`;
-
-    htmlBody += `<p>Courriel: ${customerEmail}</p>`;
-    textBody += `Courriel: ${customerEmail}\n`;
-
-    htmlBody += `<p>Numéro de Téléphone: ${phoneNumber}</p>`;
-    textBody += `Numéro de Téléphone: ${phoneNumber}\n`;
-
-    htmlBody += `<p><strong>Total Payé: $${amountPaid} CAD</strong></p>`;
-    textBody += `Total Payé: $${amountPaid} CAD\n`;
+    let textBody = `
+      Nouvelle Commande en Ligne:\n
+      Information Commande:\n
+      Numéro de Commande: ${orderNumber}\n
+      Nom: ${customerName}:\n
+      Courriel: ${customerEmail}\n
+      Numéro de Téléphone: ${phoneNumber}\n
+      Total Payé: $${amountPaid} CAD\n
+    `;
 
     panierWithoutImg.forEach(item => {
       if (item.bonbonsSelectionne) {
-        htmlBody += `<h2>${item.nom}</h2><ul>`;
+        htmlBody += `<p>${item.nom}</p><ul>`;
         textBody += `${item.nom}:\n`;
         
         item.bonbonsSelectionne.forEach(bonbon => {
@@ -61,6 +125,7 @@ const postNouvelleCommande = async (req, res) => {
       }
     });
 
+    htmlBody += `</div>`
     // Remove last newline character from textBody
     textBody = textBody.trim();
     
